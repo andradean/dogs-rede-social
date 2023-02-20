58902156b586225683d8462319c252b6179a5ed1
andradean <viniciusansilva@outlook.com>
2023-02-20 17:58:44 -0300

    photo comments concluido

--- src/components/photo/PhotoContent.jsx
+++ src/components/photo/PhotoContent.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { UserContext } from '../../Usercontext'
 import PhotoComments from './PhotoComments'
 import styles from './photoContent.module.css'
+import PhotoDelete from './PhotoDelete'
 
 const PhotoContent = ({data}) => {
   const {photo, comments} = data
+  const user = React.useContext(UserContext)
   
   return (
     <div className={styles.photo}>
@@ -13,11 +16,14 @@ const PhotoContent = ({data}) => {
       </div>
       <div className={styles.details}>
           <div>
-            <p><Link to={`/perfil/${photo.author}`}>@{photo.author}</Link> <span className={styles.visualizacoes}>{photo.acessos}</span></p>
+            <p className={styles.author}>
+              {user.data && user.data.username === photo.author ? (<PhotoDelete id={photo.id}/>) : (<Link to={`/perfil/${photo.author}`}>@{photo.author}</Link> )}
+              <span className={styles.visualizacoes}>{photo.acessos}</span>
+            </p>
             <h1 className='title'><Link to={`/foto;${photo.id}`}>{photo.title}</Link></h1>
             <ul className={styles.attributes}>
-                <li>{photo.peso}kg</li>
-                <li>{photo.idade}ano</li>
+                <li>{photo.peso} kg</li>
+                <li>{photo.idade} anos</li>
             </ul>
           </div>
       </div>
