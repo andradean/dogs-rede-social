17692b48bceeaf324254191a6e31de5ddbf98289
andradean <viniciusansilva@outlook.com>
2023-02-21 11:25:56 -0300

    feed minha conta terminado

--- src/components/feed/FeedPhotos.jsx
+++ src/components/feed/FeedPhotos.jsx
@@ -6,13 +6,13 @@ import Loading from '../helper/Loading'
 import FeedPhotosItem from './FeedPhotosItem'
 import styles from './Feedphotos.module.css'
 
-const FeedPhotos = ({setModalPhoto}) => {
+const FeedPhotos = ({ user, setModalPhoto}) => {
   
   const {data, loading, error, request} = useFetch()
   
   React.useEffect(() => {
     async function fetchPhotos () {
-      const {url, options} = PHOTOS_GET({page: 1, total: 6, user:0})
+      const {url, options} = PHOTOS_GET({page: 1, total: 3, user})
       const { response, json } = await request(url, options)
       console.log(json)
     }
--- src/components/user/User.jsx
+++ src/components/user/User.jsx
@@ -1,16 +1,19 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
+import { UserContext } from '../../Usercontext'
 import Feed from '../feed/Feed'
 import Userheader from './Userheader'
 import UserPhotoPost from './UserPhotoPost'
 import UserStats from './UserStats'
 
 const User = () => {
+  const {data} = React.useContext(UserContext)
+  
   return (
     <section className='container'>
        <Userheader />
        <Routes>
-        <Route path='/' element={<Feed />}/>
+        <Route path='/' element={<Feed user={data.id}/>}/>
         <Route path='postar' element={<UserPhotoPost />}/>
         <Route path='estatisticas' element={<UserStats />}/>
 
